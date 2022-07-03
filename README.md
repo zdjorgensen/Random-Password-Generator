40d8750b1a5b84a287b5e853741ca19fe2952d18
Zach <z.d.jorgensen@gmail.com>
2022-07-03 01:53:23 -0600

    final

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
+<div id="top"></div>
+
 # Random-Password-Generator
 
 <!-- ABOUT THE PROJECT -->
 ## About The Project
 
-<img src="#"
-alt="#" height="#">
+<img src="img/Random-Password-Generator-Screenshot.png"
+alt="Randomly generated password" height="500" width="900">
 
-(Add Text Here)
+For the third challenge, `Random Password Generator`, I took the provided HTML and CSS code and did not change them. In the JavaScript file however, I created variables that kept track of how the user responded to the prompts. I also created the prompts for the user to answer along with rules the user had to follow to generate the password. Once the user has selected at least one option from the prompts, a randomly generated password is displayed which can be seen in the picture above. I used previous activites and Google as guides while creating the JavaScript file.  
 
 <!-- BUILT WITH -->
-## Built With
+# Built With
 
 <ul>
     <li> HTML
     <li> CSS
-    <li> JAVASCRIPT
-<ul>
+    <li> JavaScript
+</ul>
 
 <!-- CONTACT -->
-## Contact
+# Contact
 
-Zachry jorgensen - z.d.jorgensen@gmail.com
+Zachry Jorgensen - z.d.jorgensen@gmail.com
 
-Project Link: [ADD LINK]
+Project Link: [https://zdjorgensen.github.io/Random-Password-Generator/]
 
 <p align="right">(<a href="#top">Back to Top</a>)</p>
--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-// Assignment code here
-
+// specialChar, numeric, lowercase, uppercase determines if user want those characters in the generated password
+var specialChar = 0;
+var numeric = 0;
+var lowercase = 0;
+var uppercase = 0;
+var passwordLength = 0;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", PasswordLength);
+
+// Determines how many characters the password will contain
+function PasswordLength(){
+  var passwordPrompt = prompt("How many characters would you like your password to contain?");
+  if(passwordPrompt < 8 || passwordPrompt > 128 ) {
+    alert("Password length must be at least 8 characters and no more than 128 characters.");
+    return;
+  } else {
+    passwordLength = passwordPrompt;
+    addCharacters();
+  } 
+}
+
+// Results will either be 0 or 1 depending on if they want those characters in the password.
+function addCharacters(){ 
+  var specialCharacterPrompt = prompt("Click OK to include special characters in your password.")
+    if(specialCharacterPrompt != null) specialChar = 1;
+
+  var numericPrompt = prompt("Click OK to include numeric characters in your password.");
+    if(numericPrompt != null) numeric = 1;
+
+  var lowercasePrompt = prompt("Click OK to include lowercase characters in your password.");
+    if(lowercasePrompt != null) lowercase = 1;
+
+  var uppercasePrompt = prompt("Click OK to include uppercase characters in your password.");
+    if(uppercasePrompt != null) uppercase = 1;
+
+  if(specialChar == 0 && numeric == 0 && lowercase == 0 && uppercase == 0){
+    alert("Must choose at least one option")
+    return;
+  }
+  writePassword();
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +50,38 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  // Resets choices for another password
+  specialChar = 0;
+  numeric = 0;
+  lowercase = 0;
+  uppercase = 0;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Generates the password depending on the user's choices 
+function generatePassword() {
+  var generatedPassword = "";
+  var possibleCharacters = "";
+
+  if(specialChar == 1) {
+    var specialCharList = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    possibleCharacters = possibleCharacters.concat(specialCharList);
+  }
+  if(numeric == 1){
+    var numericList = "1234567890";
+    possibleCharacters = possibleCharacters.concat(numericList);
+  }
+  if(lowercase == 1){
+    var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
+    possibleCharacters = possibleCharacters.concat(lowercaseList);
+  }
+  if(uppercase == 1){
+    var uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    possibleCharacters = possibleCharacters.concat(uppercaseList);
+  }
+
+  for (var i=0; i<passwordLength; i++){ //Source for the for loop: https://www.codegrepper.com/code-examples/javascript/select+random+character+from+string+javascript
+    generatedPassword += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+  }
+  
+  return generatedPassword;
+}
